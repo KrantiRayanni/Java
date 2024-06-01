26f62f39cbd6911ef945157b1eb3538774196535
KrantiRayanni <34578142+KrantiRayanni@users.noreply.github.com>
2024-06-01 08:57:35 +0530

    Initiated simple project

---
+++ JavaBasics/src/examples/day1/SimpleProgram.java
@@ -0,0 +1,21 @@
+package examples.day1;
+
+public class SimpleProgram {
+
+	public static void main(String[] args) {
+	  // This is my first program
+
+		System.out.println("Hello Java");
+
+	 // Program on Addition
+		
+		int x = 10;
+		int y = 10;
+		int z = x+y;
+	
+		System.out.println("z="+z);
+		
+
+	}
+
+}
